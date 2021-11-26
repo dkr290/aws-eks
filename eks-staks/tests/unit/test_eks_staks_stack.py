9d201c7668abfc9efa5d5a6282cfde573458d308
Danail surudzhiyski <dkrasimirov@gmail.com>
2021-11-26 18:18:36 +0100

    adding some python

---
+++ eks-staks/tests/unit/test_eks_staks_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from eks_staks.eks_staks_stack import EksStaksStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in eks_staks/eks_staks_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = EksStaksStack(app, "eks-staks")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
